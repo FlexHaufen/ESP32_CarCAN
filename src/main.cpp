a5d12da41e09addaf54b19bb39c5416145997501
FlexHaufen <ibims1k0tt@gmail.com>
2024-06-04 17:55:21 +0200

    Added initial content

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file main.cpp
+ * @author flexhaufen
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+// *** INCLUDES ***
+// external
+#include <Arduino.h>
+
+// ** DEFINES ***
+
+
+// *** GLOABL VARIABLES ***
+
+/**
+ * @brief Setup
+ * 
+ */
+void setup() {
+   
+}
+
+/**
+ * @brief Loop
+ * 
+ */
+void loop() {
+
+}
